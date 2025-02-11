5fe8a43a03f0b9cbd29aea205ed77b8ade72e6bb
PipGrylls <pipgryllsastro@gmail.com>
2025-02-11 09:31:42 +0000

    adding findphi and findq in py_roche

--- py_subs/src/py_roche.cpp
+++ py_subs/src/py_roche.cpp
@@ -20,13 +20,176 @@ void init_roche(py::module_ &m) {
     m.def("face",
             [](double q, Roche::STAR star, double spin, 
                 const Subs::Vec3& dirn, double rref, double pref, double acc) {
+                    
                 Subs::Vec3 pvec, dvec;
                 double r, g;
                 Roche::face(q, star, spin, dirn, rref, pref, acc, pvec, dvec, r, g);
                 return std::make_tuple(pvec, dvec, r, g);
             },
-            "Computes the position of a point on the Roche distorted surface",
+            "p,d,r,g = face(q, spin, dirn, rref, pref, star=2, acc=1.e-5), returns position and direction of element of specific Roche potential.
+            q      -- mass ratio = M2/M1
+            spin   -- ratio spin/orbital frequency
+            dirn   -- direction (a Vec3) to take from centre of mass of star in question.
+            rref   -- reference radius greater than any radius of potential in question.
+            pref   -- the potential to aim for.
+            star   -- 1 or 2 for primary or secondary star. acc    -- accuracy in terms of separation of location.
+
+            Returns p = position, d = direction perpendicular to face, r = radius from centre of mass, g = gravity.
+            ",
             py::arg("q"), py::arg("star"), py::arg("spin"), py::arg("dirn"), py::arg("rref"), py::arg("pref"), py::arg("acc")
     );
 
+    m.def("bspot",
+            [](double q, double rad, double acc = 1.e-7) {
+                Subs::Vec3 r, v, rs, vs;
+                Roche::strinit(q, r, v);
+                try{
+                    Roche::stradv(q, r, v, rad, acc, 1.e-2);
+                    return std::make_tuple(r.x(), r.y(), v.x(), v.y());
+                }
+                catch(const Roche::Roche_Error& err){
+                    throw std::runtime_error("roche.stradv: never achieved desired radius");
+                }
+
+            },
+            "returns position and stream velocity on stream at radius rad
+
+            (x,y,vx,vy) = bspot(q, rad, acc=1.e-7)
+
+            Args:
+                q (float): mass ratio = M2/M1
+                rad (float): radius to aim for
+                acc (float[optional]): computationa accuracy parameter",
+            py::arg("q"), py::arg("rad"), py::arg("acc") = 1.e-7
+    );    
+
+    m.def("ref_sphere",
+            [](double q, double spin, double ffac, int star = 2) {
+                double rref, pref;
+                Roche::ref_sphere(q, star == 1 ? Roche::STAR::PRIMARY : Roche::STAR::SECONDARY, spin, ffac, &rref, &pref);
+                return std::make_tuple(rref, pref);
+            },
+            "    
+            (rref,pref) = ref_sphere(q, spin, ffac, star=2), returns reference radius and potential needed for face.
+            q      -- mass ratio = M2/M1
+            spin   -- ratio spin/orbital frequency
+            ffac   -- linear filling factor of star in question, defined as the radius of the star along the line of
+                    centres towards its companion divided by the Roche lobe radius in that direction. For spin = 1
+                    the latter is simply the distance to the L1 point, but otherwise you need to use modified L1
+                    radii as returned by xl11 or xl12.
+            star   -- 1 or 2 for primary or secondary star.",
+            py::arg("q"), py::arg("spin"), py::arg("ffac"), py::arg("star") = 2
+    );
+
+    m.def("findi",
+            [](double q, double deltaphi, double acc = 1.e-4, double di = 1.e-5) {
+                // do assertion checks
+                if(q <= 0.){
+                    throw std::runtime_error("roche.findi: q must be > 0");
+                }
+                if(deltaphi <= 0. || deltaphi > 0.25){
+                    throw std::runtime_error("roche.findi: deltaphi out of range 0 to 0.25");
+                }
+                if(acc <= 0. || acc > 0.1){
+                    throw std::runtime_error("roche.findi: acc <= 0 or acc > 0.1");
+                }
+                if(di <= 0. || di > 10.){
+                    throw std::runtime_error("roche.findi: di <= 0 or di > 10.");
+                }
+
+                double ilo = 65., ihi = 90.;
+                double phi = deltaphi/2.;
+                Subs::Vec3 earth1 = Roche::set_earth(ilo, phi);
+                Subs::Vec3 earth2 = Roche::set_earth(ihi, phi);
+                Subs::Vec3 r;
+                bool elo = Roche::fblink(q, Roche::SECONDARY, 1.0, 1.0, acc, earth1, r);
+                bool ehi = Roche::fblink(q, Roche::SECONDARY, 1.0, 1.0, acc, earth2, r);
+                double iangle;
+                if(elo && ehi){
+                    iangle = -2.;
+                }else if(!elo && !ehi){
+                    iangle = -1.;
+                }else{
+                    while(ihi - ilo > di){
+                        iangle = (ilo+ihi)/2.;
+                        if(Roche::fblink(q, Roche::SECONDARY, 1.0, 1.0, acc, Roche::set_earth(iangle, phi), r))
+                            ihi = iangle;
+                        else
+                            ilo = iangle;
+                    }
+                    iangle = (ilo+ihi)/2.;
+                }
+                return iangle; 
+            },
+        "findi(q, deltaphi, acc=1.e-4, di=1.e-5), 
+        computes inclination for a given mass ratio and phase width",
+        py::arg("q"), py::arg("deltaphi"), py::arg("acc") = 1.e-4, py::arg("di") = 1.e-5
+    );
+
+    m.def("findq",
+            [](double i, double deltaphi, acc=1.e-4, dq=1.0e-5, qlo=0.001, qhi=2.){
+                // do assertion checks
+                if(i <= 0. || i > 90.){
+                    throw std::runtime_error("roche.findq: i out of range 0 to 90");
+                }
+                if(deltaphi <= 0. || deltaphi > 0.25){
+                    throw std::runtime_error("roche.findq: deltaphi out of range 0 to 0.25");
+                }
+                if(acc <= 0. || acc > 0.1){
+                    throw std::runtime_error("roche.findq: acc <= 0 or acc > 0.1");
+                }
+                if(dq <= 0. || dq > 0.1){
+                    throw std::runtime_error("roche.findq: di <= 0 or di > 0.1");
+                }
+
+                double phi = deltaphi/2.;
+                Subs::Vec3 r;
+                Subs::Vec3 earth = Roche::set_earth(i, phi);
+                bool elo = Roche::fblink(qlo, Roche::SECONDARY, 1.0, 1.0, acc, earth, r);
+                bool ehi = Roche::fblink(qhi, Roche::SECONDARY, 1.0, 1.0, acc, earth, r);
+                double q;
+                if(elo && ehi){
+                    q = -2.;
+                }else if(!elo && !ehi){
+                    q = -1.;
+                }else{
+                    while(qhi - qlo > dq){
+                        q = (qlo+qhi)/2.;
+                        if(Roche::fblink(q, Roche::SECONDARY, 1.0, 1.0, acc, earth, r))
+                            qhi = q;
+                        else
+                            qlo = q;
+                    }
+                    q = (qlo+qhi)/2.;
+                }
+                return q
+            },
+            "findi(q, deltaphi, acc=1.e-4, di=1.e-5),
+            computes mass ratio q for a given phase width and inclination",
+            py::arg("i"), py::arg("deltaphi"), py::arg("acc") = 1.e-4, py::arg("dq") = 1.e-5, py::arg("qlo") = 0.005, py::arg("qhi") = 2.
+    );
+
+    m.def("findphi",
+            [](double q, double i, double delta=1.e-6){
+                // do assertion checks
+                if(q <= 0.){
+                    throw std::runtime_error("roche.findphi: q <= 0");
+                }
+                if(i <= 0. || i > 90.){
+                    throw std::runtime_error("roche.findphi: i out of range 0 to 90");
+                }
+                if(delta <= 0. || delta > 0.001){
+                    throw std::runtime_error("roche.findphi: delta <= 0 or delta > 0.001");
+                }
+                Subs::Vec3 r(0,0,0);
+                double ingress, egress;
+                if(!Roche::ingress_egress(q, Roche::SECONDARY, 1.0, 1.0, i, delta, r, ingress, egress)){
+                    throw std::runtime_error("roche.findphi: the centre of mass of the white dwarf is not eclipsed");
+                }
+                return egress - ingress;
+            },
+            "findphi(q, i, delta=1.e-6), computes deltaphi for a given mass ratio and inclination",
+            py::arg("q"), py::arg("i"), py::arg("delta") = 1.e-6
+
+    )
 }
