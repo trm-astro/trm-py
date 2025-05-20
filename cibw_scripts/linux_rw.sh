b4a86d1354b324f5ee91307cbb73586a122b0b40
PipGrylls <pipgryllsastro@gmail.com>
2025-05-20 12:00:34 +0100

    removing lib inspection

--- cibw_scripts/linux_rw.sh
+++ cibw_scripts/linux_rw.sh
@@ -22,8 +22,6 @@ rm -rf uncomp || true
 unzip "$2" -d "uncomp"
 # inspect the three c++ extensions found in the wheel
 # check the shared object files
-echo "Checking lib"
-ls -l uncomp/lib/
 echo "Checking trm_py/_cpp"
 ls -l uncomp/trm_py/_cpp/
 
